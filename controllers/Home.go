c25d98601d75c390801de4d3b2747275b8a31e0a
Nichlas Torgersen <nichlas@coretrek.no>
2024-10-18 10:41:53 +0200

    adds more robust way of deducing IP from request

--- controllers/Home.go
+++ controllers/Home.go
@@ -6,11 +6,30 @@ import (
 	"os"
 )
 
+  /**
+  * Attempts to get the IP from the request.
+  * Falls back to RemoteAddr if IP can not be deduced from headers.
+  */
+func IpFromRequest(request *http.Request) (string, string) {
+    var ip string
+
+    if ip = request.Header.Get("X-Real-IP"); ip != "" {
+        return "X-Real-IP", ip
+    }
+
+    if ip = request.Header.Get("X-Forwarded-For"); ip != "" {
+        return "X-Forwarded-For", ip
+    }
+
+    return "RemoteAddr", request.RemoteAddr
+}
+
 func Home(writer http.ResponseWriter, request *http.Request) {
     data, err := os.ReadFile("./resources/views/home.html")
     cwd, _ := os.Getwd()
+    ipSource, ip := IpFromRequest(request)
 
-    log.Printf("Received %s from %s", request.Method, request.RemoteAddr)
+    log.Printf("Received %s from %s (%s)", request.Method, ip, ipSource)
 
     if err != nil {
         log.Printf("Could not read resources/views/home.html: %s (%s)", err.Error(), cwd) 
